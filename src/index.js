0c8a6a31beb43af0c08a93d81ae2f597db5b0b8b
Yordy Garcia <ygarciaglez1995@gmail.com>
2021-02-03 17:26:07 -0600

    df

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL || ''}>
       <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
